ef9bb68e217bb30920901d12610873fbb02655c8
smpower <bzsjxhywrf@outlook.com>
2018-08-22 17:13:43 +0800

    搭建 counter 组件基本结构和样式

--- slider/src/counter/views/counter.js
+++ slider/src/counter/views/counter.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
 
 class Counter extends React.Component {
+	constructor(props, context) {
+	  super(props, context);
+
+		this.getOwnState = this.getOwnState.bind(this);
+		this.onIncrement = this.onIncrement.bind(this);
+		this.onDecrement = this.onDecrement.bind(this);
+
+		this.state = this.getOwnState();
+	}
+
+	getOwnState() {
+	  return this.context.store.getState()
+	}
+
+	onIncrement() {
+	  console.log('onIncrement');
+	}
+
+	onDecrement() {
+	  console.log('onDecrement');
+	}
+
   render() {
+		console.log(this.state);
 	  return (
-		  <div>
-			  Counter Component
+		  <div className="counter">
+			  <i className="button icon-add" onClick={this.onIncrement}></i>
+			  <i className="button icon-reduce" onClick={this.onDecrement}></i>
+			  <span>0</span>
 			</div>
 		);
 	}
 }
+
+Counter.contextTypes = {
+  store: PropTypes.object
+};
 
 export default Counter;
