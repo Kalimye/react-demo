813beca80e6c2971f8bea029c1620172666f7978
smpower <bzsjxhywrf@outlook.com>
2018-08-22 09:45:40 +0800

    初步实现组件按需加载

---
+++ slider/src/Bundle.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Bundle extends React.Component {
+  constructor(props) {
+	  super(props);
+
+		this.state = {mod: null};
+	}
+
+	componentWillMount() {
+		console.log(this.props);
+	  this.load(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.load !== this.props.load) {
+		  this.load(nextProps);
+		}
+	}
+
+	load(props) {
+	  this.setState({mod: null});
+
+		// import() 返回一个 Promise 对象
+		props.load().then(mod => {
+		  this.setState({
+			  mod: mod.view ? mod.view : null
+			});
+		});
+	}
+
+	render() {
+	  return this.state.mod ? this.props.children(this.state.mod) : null
+	}
+}
--- slider/src/main/views/main.js
+++ slider/src/main/views/main.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {view as Home} from '../../home/';
-import {view as About} from '../../about/';
-import {view as Topic} from '../../topic/';
+// import {view as Home} from '../../home/';
+// import {view as About} from '../../about/';
+// import {view as Topic} from '../../topic/';
+import Bundle from '../../Bundle.js';
 
 import './style.css';
+
+const Home = props => {
+	return (
+		<Bundle load={() => import('../../home/')}>
+			{(Home) => <Home {...props} />}
+		</Bundle>
+	);
+};
+
+const About = props => {
+  return (
+	  <Bundle load={() => import('../../about/')}>
+		  {(About) => <About {...props} />}
+		</Bundle>
+	);
+};
+
+const Topic = props => {
+  return (
+	  <Bundle load={() => import('../../topic/')}>
+		  {(Topic) => <Topic {...props} />}
+		</Bundle>
+	);
+};
 
 class Main extends React.Component {
 	render() {
