4fdebf1e4fe75e17e159c3a6caab7c764bbd00ba
smpower <rf.wangchn@gmail.com>
2018-08-25 22:15:57 +0800

    单元测试: todo/acitons

--- todos/src/__tests__/todos/actions.test.js
+++ todos/src/__tests__/todos/actions.test.js
@@ -1,13 +1,12 @@
 import * as actions from '../../todos/actions.js';
 import * as actionTypes from '../../todos/actionTypes.js';
-import store from '../../Store.js';
 
 describe('todo/actions', () => {
-  describe('addTodo', () => {
-	  const addTodo = actions.addTodo;
+	const {addTodo, toggleTodo, removeTodo} = actions;
 
+  describe('addTodo', () => {
 		it('添加一个 todo 待办事项', () => {
-		  const text = 'first todo';
+		  const text = '一个 todo 待办事项';
 			const action = addTodo(text);
 
 			expect(action.text).toBe(text);
@@ -15,39 +14,47 @@ describe('todo/actions', () => {
 			expect(action.type).toBe(actionTypes.ADD_TODO);
 		});
 
-		it('添加多个 todo 待办事项时应该有不同的 id', () => {
-		  const textFirst = 'first todo';
-			const actionFirst = addTodo(textFirst);
+		it('添加的多个 todo 待办事项的 id 应唯一', () => {
+		  const textFirst = '第一个 todo 待办事项';
+			const textSecond = '第二个 todo 待办事项';
 
-			const textSecond = 'second todo';
-			const actionSecond = addTodo(textSecond);
+			const firstAction = addTodo(textFirst);
+			const secondAction = addTodo(textSecond);
 
-			expect(actionFirst.id !== actionSecond.id).toBe(true);
+			expect(firstAction.id !== secondAction.id).toBe(true);
 		});
 	});
 
 	describe('toggleTodo', () => {
-	  const addTodo = actions.addTodo;
-		const toggleTodo = actions.toggleTodo;
-
-		it('切换 todo 完成状态', () => {
-			const text = 'add todo';
+	  it('将 todo 待办事项完成状态标记为`已完成`状态', () => {
+		  const text = '标记状态为`已完成`';
 			const addTodoAction = addTodo(text);
-			const toggleTodoAction = toggleTodo(addTodoAction.id);
+		  const toggleTodoAction = toggleTodo(addTodoAction.id);
 
-			store.dispatch(addTodoAction);
-			store.dispatch(toggleTodoAction);
+		  expect(toggleTodoAction.type).toBe(actionTypes.TOGGLE_TODO);
+		  expect(toggleTodoAction.id).toBe(addTodoAction.id);
+		});
 
-			const state = store.getState().todos;
-			const listen = () => {
-			  state = {...state, todos: store.getState().todos};
-			};
+	  it('将 todo 待办事项完成状态标记为`未完成`状态', () => {
+		  const text = '标记状态为`未完成`';
+			const addTodoAction = addTodo(text);
 
-			store.subscribe(listen);
+			toggleTodo(addTodoAction.id);
+		  const toggleTodoAction = toggleTodo(addTodoAction.id);
 
-			state.map(todo => {
-				expect(todo.completed === !addTodoAction.completed).toBe(true);
-			});
+		  expect(toggleTodoAction.type).toBe(actionTypes.TOGGLE_TODO);
+		  expect(toggleTodoAction.id).toBe(addTodoAction.id);
+		});
+	});
+
+	describe('removeTodo', () => {
+	  it('移除一个 todo 待办事项', () => {
+		  const text = '移除一个 todo 待办事项';
+			const addTodoAction = addTodo(text);
+		  const removeTodoAction = removeTodo(addTodoAction.id);
+
+		  expect(removeTodoAction.type).toBe(actionTypes.REMOVE_TODO);
+		  expect(removeTodoAction.id).toBe(addTodoAction.id);
 		});
 	});
 });
