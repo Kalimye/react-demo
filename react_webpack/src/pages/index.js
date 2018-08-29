8489f17dd0cd74622011b90290163aca5639338b
smpower <bzsjxhywrf@outlook.com>
2018-08-29 18:06:13 +0800

    markdown 支持

--- react_webpack/src/pages/index.js
+++ react_webpack/src/pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import MarkdownIt from 'markdown-it';
+import TestMarkdown from '../test.md';
+
+const md = new MarkdownIt();
 
 class Index extends React.Component {
   render() {
+		const result = md.render(TestMarkdown);
+		console.log(result);
+		console.log(md);
 	  return (
-		  <h1>This is Index page.</h1>
+			<div>
+			  <div dangerouslySetInnerHTML={{__html:result}} />
+				<h1>This is Index page.</h1>
+			</div>
 		);
 	}
 }
---
+++ react_webpack/src/test.md
@@ -0,0 +1,43 @@
+# 一级标题
+
+## 二级标题
+
+### 三级标题
+
+#### 四级标题
+
+##### 五级标题
+
+###### 六级标题
+
+这是段落
+
+超链接：[百度一下，你就知道](https://www.baidu.com/)
+
+有序列表示例
+1. 列表一
+2. 列表二
+3. 列表三
+
+无序列表示例
+* 列表一
+* 列表二
+* 列表三
+
+无序列表的另一种形式
+- 列表一
+- 列表二
+- 列表三
+	- 子列表
+	- 子列表
+
+这是一个图片的示例
+![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)
+
+```javascript
+const name = 'Olive';
+const getName = (name) => {
+  console.log(name);	
+};
+getName(name);
+```
